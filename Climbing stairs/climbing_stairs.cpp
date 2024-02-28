41277d3da00aff41d942dcb68945ccb4e0becb19
RenatoMignone <renato.mignone@gmail.com>
2024-02-28 20:25:23 +0100

    climbing stars cpp

--- Climbing stairs/climbing_stairs.cpp
+++ Climbing stairs/climbing_stairs.cpp
@@ -4,12 +4,25 @@ using namespace std;
 class Solution {
 public:
     int climbStairs(int n) {
-        
+        int result = 0;
+        if(n%2){
+            int i = (n/2)-1;
+            for(i;i>1;i--){
+                result += i*2;
+                result += (n-(i*2))*2;
+            }
+            return result + 1 + (n/2) + n;
+        }
+        else{
+            
+        }
+        return 0;
+
     }
 };
 
 int main(){
 
-    
-
+    Solution soluzione;
+    cout << soluzione.climbStairs(9);
 }
