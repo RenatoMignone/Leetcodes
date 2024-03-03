a0c0adc9c3b5793318e8e3898609ca70e9687c9a
RenatoMignone <renato.mignone@gmail.com>
2024-03-03 11:48:40 +0100

    .

--- Easy/Climbing stairs/climbing_stairs.cpp
+++ Easy/Climbing stairs/climbing_stairs.cpp
@@ -3,21 +3,19 @@ using namespace std;
 
 class Solution {
 public:
-    int climbStairs(int n) {
-        int result = 0;
-        if(n%2){
-            int i = (n/2)-1;
-            for(i;i>1;i--){
-                result += i*2;
-                result += (n-(i*2))*2;
-            }
-            return result + 1 + (n/2) + n;
+    int climbStairs(int n, unordered_map<int, int>& memo) {
+        if (n == 0 || n == 1) {
+            return 1;
         }
-        else{
-            
+        if (memo.find(n) == memo.end()) {
+            memo[n] = climbStairs(n-1, memo) + climbStairs(n-2, memo);
         }
-        return 0;
+        return memo[n];
+    }
 
+    int climbStairs(int n) {
+        unordered_map<int, int> memo;
+        return climbStairs(n, memo);
     }
 };
 
